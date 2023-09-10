901c2f9bbbd83d1f3eaf10b719b60a6d03a8f819
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-09-10 14:15:55 -0700

    just for a test , project 0

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	puts("Hello world");
+	return (0);
+}
