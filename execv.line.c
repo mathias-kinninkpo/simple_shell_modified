76c835f9e8cf520e52bda74bc6cbe8057a37366b
Prince <prince.affum45@gmail.com>
2023-09-13 05:04:27 -0700

    shell

---
+++ execv.line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * exec_line - function is responsible for determining whether the
+ * input command is a bulit-in command or an external command
+ * and then dispatching the appropriate action
+ * @inptsh: pointer to the data structure (argt)
+ * that holds command- related information
+ * Return: 1 on success
+ */
+int exec_line(inpt_shell *inptsh)
+{
+	int (*builtin)(inpt_shell *inptsh);
+
+	if (inptsh->argt[0] == NULL)
+		return (1);
+	builtin = get_builtin(inptsh->argt[0]);
+
+	if (builtin != NULL)
+		return (builtin(inptsh));
+
+	return (cmd_execut(inptsh));
+}
